71f9a85ed6e4152b8cbdd85cd6756d6a9d0bc658
Sreekar33 <cs14btech11033@iith.ac.in>
2017-03-18 16:43:14 +0530

    final

--- semaphore.c
+++ semaphore.c
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * $Id: semaphore.c,v 1.1 2012-05-12 19:48:02-07 dmfrank - $
+ * Derek Frank, dmfrank@ucsc.edu
+ * Harrison Vuong, hvuong@ucsc.edu
+ * David Zou, dzou@ucsc.edu
+ *
+ * NAME
+ *   do_semaphore.c - implementation file
+ *
+ * DESCRIPTION
+ *   An implementation file for implementing semaphore system calls.  These
+ *   calls are handled by the process manager server.  Semaphores are used to
+ *   both effectively allow mutual exclusion on resources and prevent race
+ *   conditions.
+ *
+ *   Semaphores are identified by a positive (non-zero) integer that's provided
+ *   when the semaphore is created; if the call to create a semaphore is passed
+ *   the value 0, the kernel chooses an identifier for the semaphore.  When the
+ *   user is done with a sempahore, the code must explicitly release the
+ *   semaphore, allowing the process manager to reuse it.
+ *
+ *   The process manager only has to handle up to 100 semaphores at any given
+ *   time, so you can simply create an array that holds the semaphores the
+ *   kernel may allocate to use.  However
+ *
+ ******************************************************************************/
+
 #include "pm.h"
 #include "param.h"
 #include "glo.h"
@@ -9,6 +36,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// constants
 #define NR_SEMS      100          // number of semaphores available in kernel
 #define SZ_PLIST     1000         // size of each semaphore's process list
 #define NO_SEM      -1            // a semaphore id set to -1 is unallocated
@@ -20,10 +48,19 @@
 #define RETERR       0            // functions return 0 on error, except do_semvalue
 
 
+// A private function within semaphore.c
 FORWARD _PROTOTYPE( int sem_exists, (int sem)    			);
 FORWARD _PROTOTYPE( void init_sems, (void)      			);
 
 
+/***************************** Type Definitions *******************************/
+/********************************** Globals ***********************************/
+
+////
+// semaphore - struct
+//
+//   A global array of semaphore structures.  Statically allocated.
+//
 struct semaphore {
    int id, value; // semaphore id and value
    int plist[SZ_PLIST]; // "queue" of sleeping processes
@@ -50,13 +87,26 @@ PRIVATE void init_sems (void) {
 }
 
 
+
+/***************************** Access Functions *******************************/
+
+////
+// sem_exists
+//
+//   Returns 1 if the semaphore id exists in the global semaphore array.
+//   Otherwise, returns 0.
+//
 PRIVATE int sem_exists (sem)
    int sem;
 {
+   // ERROR CHECKING //
+   // errors: EINVAL
+   //   check if id is negative
    if (sem <= 0 || sem > INT_LIMIT - 1) return 0;
 
+   // semaphore id cannot exist if there are no allocated semaphores
    if (semcount < 1) return 0;
-
+   // look for the semaphore id
    int i;
    for (i = 0; i < NR_SEMS; ++i) {
       if (semarray[i].id == sem) return 1; // found the id
@@ -66,12 +116,26 @@ PRIVATE int sem_exists (sem)
 }
 
 
+////
+// do_semvalue
+//
+//   This call returns the current value of the semaphore whose identifier is
+//   passed.  If there are n processes waiting on the semaphore, the call should
+//   return -n.  If the semaphore has a value of 0, the next process to call
+//   semdown() would wait, but no process is currently waiting.  If an error
+//   occurs, return 0x8000000.
+//
 PUBLIC int do_semvalue (void) {
    int exitstatus = ESEMVAL; // not yet successful, assume error
+   // get message
    int sem = m_in.m1_i1;
 
+   // ERROR CHECKING //
+   //   check if semaphore id is within bounds
+   //   semaphore cannot exist if there are none allocated
    if (sem <= 0 || sem > INT_LIMIT - 1 || semcount < 1) return ESEMVAL;
    
+   // find index around sem modulo NR_SEMS
    int index;
    index = (sem % NR_SEMS) - 1;
    if (index < 0) index = NR_SEMS - 1;
@@ -82,6 +146,8 @@ PUBLIC int do_semvalue (void) {
       else ++index;
    }
 
+   // MORE ERROR CHECKING //
+   // semaphore id could not be found
    if (exitstatus == ESEMVAL) return ESEMVAL;
 
    return exitstatus;
@@ -89,7 +155,37 @@ PUBLIC int do_semvalue (void) {
 
 
 
+/************************** Manipulation Functions ****************************/
+
+////
+// do_seminit
+//
+//   The call initializes a new semaphore.  The semaphore is identified by the
+//   integer passed in sem; if sem is 0, the kernel chooses an identifier.  Note
+//   that an identifier can be any positive integer between 1 and 2^31; it need
+//   not be in the range 1-100.  The call returns the identifier for the
+//   semaphore--either the value passed or the one the kernel chose--or 0 if an
+//   error ocurred.  The initial value for the semaphore is passed in value.
+//   On an error, the semaphore is not initialized.
+//
+//   Error conditions (with error codes) include:
+//     EAGAIN
+//       No free semaphores in the kernel (there is a limit of 100 active
+//       semaphores in the kernel).
+//
+//     EINVAL
+//       Semaphore identifier is negative.
+//
+//     EINVAL
+//       Semaphore initial value is not in the range
+//       -INIT_SEMVAL<=value<=INIT_SEMVAL.
+//
+//     EEXIST
+//       Semaphore identifier is already in use for an active semaphore (one
+//       that was freed with semfree() is not active).
+//
 PUBLIC int do_seminit (void) {
+   // initialize semaphore array if not yet initialized
    if (semcount == 0) init_sems ();
 
    // return value: should be set to the sem id or 0 if an error occurred
@@ -98,21 +194,38 @@ PUBLIC int do_seminit (void) {
    int sem = m_in.m1_i1;
    int value = m_in.m1_i2;
 
+   // ERROR CHECKING //
+   // errors: EAGAIN
+   //   check if free semaphores are available
    if (semcount >= NR_SEMS) {
       m_in.m_type = EAGAIN;
       return RETERR;
    }
+   // errors: EINVAL
+   //   check if id is negative
+   //   check if id exceeds 2^31
+   //   check if value is less than -INIT_SEMVAL
+   //   check if value is greater than INIT_SEMVAL
    if (sem < 0 || sem > INT_LIMIT - 1 || value > INIT_SEMVAL || value < -INIT_SEMVAL) {
       m_in.m_type = EINVAL;
       return RETERR;
    }
+   // errors: EEXIST
+   //   check if id is in use for active semaphore
    if (sem != 0 && sem_exists (sem)) {
       m_in.m_type = EEXIST;
       return RETERR;
    }
+
+   // allocate the semaphore
+   // two cases:
+   //   choose semaphore id, sem == 0
+   //   given semaphore id, sem != 0
    int index;
    if (sem == 0) {
+      // look for an empty/unused semaphore
       int i;
+      // only NR_SEMS available semaphores
       for (i = 0; i < NR_SEMS && exitstatus == RETERR; ++i) {
          if (semarray[i].id == NO_SEM) {
             index = i;
@@ -132,6 +245,8 @@ PUBLIC int do_seminit (void) {
          }
       }
    }else {
+      // Only have NR_SEMS indices to index, so a large id number will exist
+      // around id modulo NR_SEMS.
       index = ((sem % NR_SEMS) - 1);
       if (index < 0) index = NR_SEMS - 1;
       // find an unused semaphore
@@ -157,11 +272,28 @@ PUBLIC int do_seminit (void) {
 }
 
 
+////
+// semup
+//
+//   This call does UP on the semaphore whose identifier is passed.  This call
+//   never blocks.  If there's at least one process waiting on this semaphore,
+//   semup() causes one waiting process to be awakened.  The call returns 1 if
+//   successful, 0 otherwise.
+//
+//   NOTE: while a semaphore can't be initialized outside the range
+//   -1000<=value<=1000, it may be incremented (or decremented) to a value
+//   outside this range, up to +/-10^6.  If the semup() call would result in a
+//   value above 10^6, return 0 and set the error to EOVERFLOW.
+//
 PUBLIC int do_semup (void) {
    // return 1 if successful, 0 otherwise
    int exitstatus = RETERR; // not yet successful
    // get message
    int sem = m_in.m1_i1;
+
+   // ERROR CHECKING //
+   //   check if semaphore id is within bounds
+   //   semaphore cannot exist if there are none allocated
    if (sem <= 0 || sem > INT_LIMIT - 1 || semcount < 1) {
       m_in.m_type = EINVAL;
       return RETERR;
@@ -224,15 +356,33 @@ PUBLIC int do_semup (void) {
 }
 
 
+////
+// do_semdown
+//
+//   This call does DOWN on the semaphore whose identifier is passed.  If the
+//   semaphore value would go below zero, the call blocks until the value goes
+//   above zero again.  The call returns 1 if successful, 0 otherwise.
+//
+//   NOTE: while a semaphore can't be initialized outside the range
+//   -1000<=value<=1000, it may be incremented (or decremented) to a value
+//   outside this range, up to +/-10^6.  If the semdown() call would result in
+//   a value below -10^6, return 0 and set the erro to EOVERFLOW.
+//
 PUBLIC int do_semdown (void) {
+   // return 1 if successful, 0 otherwise
    int exitstatus = RETERR; // not yet successful
+   // get message
    int sem = m_in.m1_i1;
 
+   // ERROR CHECKING //
+   //   check if semaphore id is within bounds
+   //   semaphore cannot exist if there are none allocated
    if (sem <= 0 || sem > INT_LIMIT - 1 || semcount < 1) {
       m_in.m_type = EINVAL;
       return RETERR;
    }
    
+   // find index around sem modulo NR_SEMS
    int index;
    index = (sem % NR_SEMS) - 1;
    if (index < 0) index = NR_SEMS - 1;
@@ -243,6 +393,8 @@ PUBLIC int do_semdown (void) {
       else ++index;
    }
 
+   // MORE ERROR CHECKING //
+   // semaphore id could not be found
    if (exitstatus == RETERR) {
       //m_in.m_type = EEXIST;
       return RETERR;
@@ -277,7 +429,10 @@ PUBLIC int do_semdown (void) {
          semarray[index].end = last;
          semarray[index].plist[last] = proc_nr;
       }
+      // increment process list count
       ++(semarray[index].pcount); // one more sleeping process
+      
+      // suspend this process, i.e., put to sleep
       return (SUSPEND);
    }
 
@@ -285,11 +440,32 @@ PUBLIC int do_semdown (void) {
 }
 
 
+////
+// do_semfree
+//
+//   This call frees a semaphore that's currently allocated, making the slot in
+//   the kernel available for reuse.  A semaphore may not be released if there
+//   are processes waiting on it; the process that wants to free the semaphore
+//   must call semup() if necessary to ensure that there are no waiting
+//   processes before freeing it.  The call returns 1 if successful, 0
+//   otherwise.
+//
+//   Error codes include:
+//     EBUSY
+//       The semaphore has process(es) waiting on it.
+//
+//     EEXIST
+//       The semaphore doesn't currently exist.
+//
 PUBLIC int do_semfree (void) {
    // return 1 if successful, 0 otherwise
    int exitstatus = RETERR; // not yet successful
    // get message
    int sem = m_in.m1_i1;
+   // ERROR CHECKING //
+   // errors: EEXIST
+   //   check if id is negative or 0
+   //   check if id exceeds 2^31
    if (sem <= 0 || sem > INT_LIMIT - 1) {
       m_in.m_type = EINVAL;
       return RETERR;
@@ -306,10 +482,15 @@ PUBLIC int do_semfree (void) {
       else ++index; // increment the index
    }
 
+   // MORE ERROR CHECKING //
+   // errors: EEXIST
+   //   could not find the semaphore
    if (exitstatus == RETERR) {
       m_in.m_type = EEXIST;
       return RETERR;
    }
+   // errors: EBUSY
+   //   check if the semaphore has processes waiting on it
    if (semarray[index].pcount != 0) {
       m_in.m_type = EBUSY;
       return RETERR;
@@ -328,4 +509,3 @@ PUBLIC int do_semfree (void) {
    
    return exitstatus;
 }
-
